b04cd3bd6b515665ec6690bdc1c74b1534c9c2f9
lipei <lipei@supos.com>
2021-04-13 16:55:53 +0800

    t

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,7 +11,12 @@ module.exports = {
         path: `${__dirname}/src/`,
       },
     },
-    `gatsby-transformer-remark`,
     'gatsby-plugin-emotion',
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
+      },
+    },
   ],
 };
